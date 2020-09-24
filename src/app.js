5f268161a0cbeec3d41ca99d26615e74a392b118
frantracer <frantracer@gmail.com>
2020-09-24 14:53:03 +0200

    Add sample generation from template

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const nunjucks = require('nunjucks')
+const fs = require("fs");
 
-console.log("hola mundo")
+try {
+    console.log("Reading template...")
+    const input = fs.readFileSync("./examples/templates/basic/index.html").toString();
 
-var template = nunjucks.compile('Hello {{ username }}');
-var output = template.render({ username: 'James' });
+    console.log("Reading profile...")
+    const profile = JSON.parse(fs.readFileSync("./examples/profiles/basic/profile1.json").toString());
 
-console.log(output)
+    console.log("Checking template...")
+    const template = nunjucks.compile(input);
+
+    console.log("Generating file...")
+    const output = template.render(profile);
+
+    console.log("File generated...")
+    console.log(output)
+
+    console.log("Saving file...")
+    fs.writeFileSync("./examples/outputs/basic/index+profile1.html", output)
+} catch (e) {
+    console.log(e.toString())
+}
