84a0fb9653d6fff1738d4cad2dafc06c64b00c8b
frantracer <frantracer@gmail.com>
2020-09-24 19:34:02 +0200

    Use a loop to generate files for several profiles

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
 const nunjucks = require('nunjucks')
 const fs = require("fs")
+const path = require('path')
 
 try {
-    console.log("Reading template...")
-    const input = fs.readFileSync("./examples/templates/basic/index.html").toString()
+    let templates_dir = "./examples/basic/templates"
+    let profiles_dir = "./examples/basic/profiles"
+    let outputs_dir = "./examples/basic/outputs"
 
-    console.log("Reading profile...")
-    const profile = JSON.parse(fs.readFileSync("./examples/templates/basic/profiles/profile1.json").toString())
+    let template_filenames = fs.readdirSync(templates_dir)
+    template_filenames.forEach((template_filename) => {
+        console.log(`Reading template: ${template_filename}`)
 
-    console.log("Checking template...")
-    const template = nunjucks.compile(input)
+        let template_extension = path.extname(template_filename)
+        let template_name = path.basename(template_filename, template_extension)
 
-    console.log("Generating file...")
-    const output = template.render(profile)
+        const input = fs.readFileSync(templates_dir + "/" + template_filename).toString()
+        const renderer = nunjucks.compile(input)
 
-    console.log("File generated...")
-    console.log(output)
+        let profile_filenames = fs.readdirSync(profiles_dir)
+        profile_filenames.forEach((profile_filename) => {
+            console.log(`\tReading profile: ${profile_filename}`)
+            let profile = JSON.parse(fs.readFileSync(profiles_dir + "/" + profile_filename).toString())
+            let profile_name = path.basename(profile_filename, ".json")
 
-    console.log("Saving file...")
-    fs.writeFileSync("./examples/outputs/basic/index+profile1.html", output)
+            let output = renderer.render(profile)
+
+            let output_path = outputs_dir + "/" + template_name + "+" + profile_name + template_extension
+            fs.writeFileSync(output_path, output)
+            console.log(`\t\tNew file saved in ${output_path}`)
+        })
+    })
 } catch (e) {
     console.log(e.toString())
 }
