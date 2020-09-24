9d8325576be20a1cb694b443773ee9c096fd3cd9
frantracer <frantracer@gmail.com>
2020-09-24 14:06:17 +0200

    Add nunjucks dependency

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+const nunjucks = require('nunjucks')
+
 console.log("hola mundo")
+
+var template = nunjucks.compile('Hello {{ username }}');
+var output = template.render({ username: 'James' });
+
+console.log(output)
