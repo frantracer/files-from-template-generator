9d01211f8921178fbe1b1fde70e90df891eb0555
frantracer <frantracer@gmail.com>
2020-09-24 16:02:42 +0200

    Remove semicolons

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const nunjucks = require('nunjucks')
-const fs = require("fs");
+const fs = require("fs")
 
 try {
     console.log("Reading template...")
     const input = fs.readFileSync("./examples/basic/templates/index.html").toString();
 
     console.log("Reading profile...")
-    const profile = JSON.parse(fs.readFileSync("./examples/basic/profiles/profile1.json").toString());
+    const profile = JSON.parse(fs.readFileSync("./examples/basic/profiles/profile1.json").toString())
 
     console.log("Checking template...")
-    const template = nunjucks.compile(input);
+    const template = nunjucks.compile(input)
 
     console.log("Generating file...")
-    const output = template.render(profile);
+    const output = template.render(profile)
 
     console.log("File generated...")
     console.log(output)
