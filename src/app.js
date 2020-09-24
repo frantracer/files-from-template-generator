bee7dc03d32ccaa9423671f644a6bf0066a3a4a8
frantracer <frantracer@gmail.com>
2020-09-24 16:23:22 +0200

    Reorganize directories

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 
 try {
     console.log("Reading template...")
-    const input = fs.readFileSync("./examples/basic/templates/index.html").toString()
+    const input = fs.readFileSync("./examples/templates/basic/index.html").toString()
 
     console.log("Reading profile...")
-    const profile = JSON.parse(fs.readFileSync("./examples/basic/profiles/profile1.json").toString())
+    const profile = JSON.parse(fs.readFileSync("./examples/templates/basic/profiles/profile1.json").toString())
 
     console.log("Checking template...")
     const template = nunjucks.compile(input)
@@ -18,7 +18,7 @@ try {
     console.log(output)
 
     console.log("Saving file...")
-    fs.writeFileSync("./examples/basic/outputs/index+profile1.html", output)
+    fs.writeFileSync("./examples/outputs/basic/index+profile1.html", output)
 } catch (e) {
     console.log(e.toString())
 }
